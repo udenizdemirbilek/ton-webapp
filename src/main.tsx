0a1947f04fc62b046d45552360c2845edf348841
demirbilek <deniz.demirbilek@adesso.com.tr>
2023-02-03 17:12:34 +0300

    second commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 // this manifest is used temporarily for development purposes
 const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+  "https://udenizdemirbilek.github.io/ton-webapp/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
